e90a41b844eb4c38656a7e5aca9952009c6482db
atharv4git <atharv4study@gmail.com>
2023-06-11 09:38:30 +0530

    push-1

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+from setuptools.command.install import install
+import subprocess
+
+
+class NLTKDownloadCommand(install):
+    def run(self):
+        import nltk
+        nltk.download('punkt')
+        nltk.download('averaged_perceptron_tagger')
+        nltk.download('wordnet')
+        nltk.download('maxent_ne_chunker')
+        install.run(self)
+
+
+setuptools.setup(
+    # Other setup configurations...
+    cmdclass={
+        '': NLTKDownloadCommand,
+    },
+)
