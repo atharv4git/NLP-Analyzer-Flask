afd174979b7c5a369c28f1950be197cd6297efad
atharv4git <atharv4study@gmail.com>
2023-06-11 09:42:26 +0530

    push-1

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ class NLTKDownloadCommand(install):
 setuptools.setup(
     # Other setup configurations...
     cmdclass={
-        '': NLTKDownloadCommand,
+        'install': NLTKDownloadCommand,
     },
 )
